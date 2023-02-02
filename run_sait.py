0b8bd4b7af5b3505e9985039f1e585fd4ba56f43
DMITRY <borisovd58@gmail.comgit config --global user.email borisovd58@gmail.com>
2023-02-02 12:57:59 +0400

    dz_28_01_23

--- run_sait.py
+++ run_sait.py
@@ -1,7 +1,7 @@
 import sqlite3
 import os
-from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
-
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
+from FDataBase import FDataBase
 
 DATABASE = '/tmp/flsk.db'
 DEBUG = True
@@ -36,11 +36,48 @@ menu = [
 ]
 
 
+def get_db():                       # возвращает активное соединение с БД
+    if not hasattr(g, 'link_db'):   # link_db - переменная из g,стандартная запись- проверка на наличие аттрибутов
+        g.link_db = connect_db()
+    return g.link_db
+
+
 @app.route("/index")
 @app.route("/")
 def index():
-    print(url_for("index"))
-    return render_template("/index.html", title="Продажа, покупка и сервис автомобилей", menu=menu)
+    db = get_db()
+    dbase = FDataBase(db)  # dbase - экзэмпляр класса FDataBase
+    return render_template("/index.html", title="Продажа, покупка и сервис автомобилей", menu=dbase.get_menu(),
+                           posts=dbase.get_posts_anonce())
+
+
+@app.route("/add_post", methods=["POST", "GET"])
+def add_post():
+    db = get_db()
+    dbase = FDataBase(db)
+
+    if request.method == "POST":
+        if len(request.form['name']) > 4 and len(request.form['post']) > 10:
+            res = dbase.add_post(request.form['name'], request.form['post'], request.form['url'])
+            if not res:
+                flash("Ошибка добавления статьи", category="error")
+            else:
+                flash("Статья добавлена успешно", category="success")
+        else:
+            flash("Ошибка добавления статьи", category="error")
+
+    return render_template("add_post.html", menu=dbase.get_menu(), title="Добавление статьи")
+
+
+@app.route("/post/<alias>")
+def show_post(alias):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.get_post(alias)
+    if not title:
+        abort(404)
+
+    return render_template('post.html', menu=dbase.get_menu(), title=title, post=post)
 
 
 @app.route("/sall")
@@ -90,7 +127,16 @@ def login():
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("page404.html", title="Страница не найдена", menu=menu)
+    db = get_db()
+    dbase = FDataBase(db)
+
+    return render_template("page404.html", title="Страница не найдена", menu=dbase.get_menu())
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
 
 
 if __name__ == "__main__":
